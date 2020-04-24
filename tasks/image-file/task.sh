d04e48c9e33ef22810fca8fae17cc92ace6abc42
Caleb Washburn <cwashburn@pivotal.io>
2020-04-24 10:59:23 -0700

    don't unpack and import image into docker

--- tasks/image-file/task.sh
+++ tasks/image-file/task.sh
@@ -6,4 +6,8 @@ max_concurrent_downloads=3
 max_concurrent_uploads=3
 start_docker ${max_concurrent_downloads} ${max_concurrent_uploads}
 
+image_file="$(find image/*.{tgz} 2>/dev/null | head -n1)"
+echo "importing ${image_file} as existing:latest"
+docker import "${image_file}" existing:latest
+
 stop_docker
