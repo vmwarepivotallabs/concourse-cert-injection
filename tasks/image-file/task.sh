d27b169736b65da25149610ff56c4674bfcd8e58
Caleb Washburn <cwashburn@pivotal.io>
2020-04-24 11:03:14 -0700

    fix find command

--- tasks/image-file/task.sh
+++ tasks/image-file/task.sh
@@ -6,7 +6,7 @@ max_concurrent_downloads=3
 max_concurrent_uploads=3
 start_docker ${max_concurrent_downloads} ${max_concurrent_uploads}
 
-image_file="$(find image/*.{tgz} 2>/dev/null | head -n1)"
+image_file="$(find image/*.tgz 2>/dev/null | head -n1)"
 echo "importing ${image_file} as existing:latest"
 docker import "${image_file}" existing:latest
 
