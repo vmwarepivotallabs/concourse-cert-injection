574145b87046059b2763ef77da3444580c38d3f3
Caleb Washburn <cwashburn@pivotal.io>
2020-04-24 12:42:17 -0700

    update to export from running container

--- tasks/image-file/task.sh
+++ tasks/image-file/task.sh
@@ -7,19 +7,25 @@ max_concurrent_uploads=3
 start_docker ${max_concurrent_downloads} ${max_concurrent_uploads}
 
 image_file="$(find image/*.tgz 2>/dev/null | head -n1)"
-echo "importing ${image_file} as existing:latest"
-docker import "${image_file}" existing:latest
+echo "importing ${image_file} as current:latest"
+docker import "${image_file}" current:latest
 
 pushd source/docker
-   docker build --build-arg ca_pem=${CA_PEM} -t new-image:latest .
+   docker build --build-arg ca_pem=${CA_PEM} -t modified:latest .
    docker images
 popd
 
-pushd modified-image
-  docker save -o latest.tgz new-image:latest
-  tar -xvf latest.tgz
-  rm latest.tgz
-popd 
+# tempdir=$(mkdir -d)
+# docker save -o ${tempdir}/image modified:latest
+
+docker run \
+    --cidfile=/tmp/container.cid \
+    -v /opt/resource/print-metadata:/tmp/print-metadata \
+    --entrypoint /tmp/print-metadata  \
+    modified:latest > modified/metadata.json
+
+mkdir -p modified/rootfs/
+docker export $(cat /tmp/container.cid) | tar --exclude="dev/*" -xf - -C modified/rootfs/
 
 
 stop_docker
