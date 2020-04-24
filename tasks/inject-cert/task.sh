be71bbc88a60df37603968ecb2acf3e67835339f
Caleb Washburn <cwashburn@pivotal.io>
2020-04-24 10:50:46 -0700

    adding required args to start_docker

--- tasks/inject-cert/task.sh
+++ tasks/inject-cert/task.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source /opt/resource/common.sh
-start_docker
+
+max_concurrent_downloads=3
+max_concurrent_uploads=3
+start_docker ${max_concurrent_downloads} ${max_concurrent_uploads}
 
 stop_docker
